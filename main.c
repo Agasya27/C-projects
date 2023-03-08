98a2ff7a97465fe4512cef1f56beb70d55d678ca
Agasya27 <127315310+Agasya27@users.noreply.github.com>
2023-03-08 19:01:31 +0530

    hotel management system

--- main.c
+++ main.c
@@ -1,215 +1,220 @@
 #include <stdio.h>
 #include <stdlib.h>
-void stu()
+int hotel()
 {
-    printf("::MENU::\n");
-    printf("1.Add student\n");
-    printf("2.Show All student\n");
-    printf("3.Result\n");
-    printf("4.update student data\n");
-    printf("0.Exit\n");
+    printf("welcome to hotel asterisc\n");
+    printf( " :: MENU :: \n");
+    printf("1.samosa 30/- \n");
+    printf("2.Dosa 40/- \n");
+    printf("3. tea 10/- \n");
+    printf("4.coffee 20/- \n");
+    printf("0.exit  \n");
+
+    return 0;
+
 
 }
-struct student
-{
-    int rollno;
-    char name[50] ;
-    int z;
-    int b;
-    int c;
-
-};
-
 int main()
 
-{    int h,i,g,sum=0,j,m,v,ch,f;
-    float per;
-     struct student a[100];
+{
+    hotel();
+    int a=0,b=0,sum=0,amt=0,y=0,u=0,o=0,p=0,n=0,g=0,h=0,token=0,pay=0,z=0,eamt=0,w=0,q=0,t=0;
+    printf("enter your choice :");
+    scanf("%d",&a);
 
-     stu();
-     printf("enter your choice\n");
-     scanf("%d",&h);
+    if(a==1)
+    {
+       printf("you have selected samosa\n");
+       printf("how many plates:");
+       scanf("%d",&b);
+       y=30;
+       t=30*b;
+       sum=t;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
 
-     if(h==1)
-     printf("you have selected to add student\n");
-     else if(h==2)
-     printf("you have selected show all student\n");
-     else if(h==3)
-    printf("you have selected show result\n");
-    else if(h==4)
-    printf("you have selected update student data\n");
-    if(h==1)
-    {
-    printf("enter the size of student required:\n");
-    scanf("%d",&g);
     }
+    else if(a==2)
+    {
+       printf("you have selected dosa\n");
+        printf("how many plates:");
+       scanf("%d",&n);
+       amt=40;
+       t=40*n;
+       sum=t;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
 
-    if(g>i)
-    {
-    for(i=0;i<g;i++)
-    {
-    printf("enter name of student:\n");
-    scanf("%s",&a[i].name);
-    printf("enter roll no of student:\n");
-    scanf("%d",&a[i].rollno);
-    printf("enter marks of data structure:\n");
-    scanf("%d",&a[i].z);
-    printf("enter marks of object oriented:\n");
-    scanf("%d",&a[i].b);
-    printf("enter marks of data base management:\n");
-    scanf("%d",&a[i].c);
-     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
     }
-    }
-    for(i=0;i<g;i++)
+    else if(a==3)
     {
-    printf("name:%s\n",a[i].name);
-    printf("roll no:%d\n",a[i].rollno);
-    printf("marks of dsu:%d\n",a[i].z);
-    printf("marks of OOP:%d\n",a[i].b);
-    printf("marks of DBMS:%d\n",a[i].c);
-    printf("\n Record saved!\n");
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+       printf("you have selected tea\n");
+        printf("how many plates:");
+       scanf("%d",&g);
+       o=10;
+       t=10*g;
+       sum=t;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
     }
-   if(h==0)
-   {
-    printf("you selected have exit\n\nthank you!");
+    else if(a==4)
+    {
+       printf("you have selected coffee\n");
+        printf("how many plates:");
+       scanf("%d",&h);
+       p=20;
+       t=20*h;
+       sum=t;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
+
+    }
+    else if(a==0)
+    {
+       printf("you have selected exit\n");
+    }
+    else
+    {
+        printf("nothing\n",a);
+    }
+    if(a>0)
+    {
+   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+   printf("\n");
+
+
+     hotel();
+    printf("enter your choice :");
+    scanf("%d",&w);
    }
-   if(h>=1)
+
+
+    if(w==1)
+    {
+       printf("you have selected samosa\n");
+        printf("how many plates:");
+       scanf("%d",&b);
+       y=30;
+       sum=t+30*b;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
+
+    }
+    if(w==2)
+    {
+       printf("you have selected dosa\n");
+        printf("how many plates:");
+       scanf("%d",&n);
+       amt=40;
+       sum=t+40*n;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
+    }
+    if(w==3)
+    {
+       printf("you have selected tea\n");
+        printf("how many plates:");
+       scanf("%d",&g);
+       o=10;
+       sum=t+10*g;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
+    }
+    if(w==4)
+    {
+       printf("you have selected coffee\n",a);
+        printf("how many plates:");
+       scanf("%d",&h);
+       p=20;
+       sum=t+20*h;
+       printf("\n");
+   printf("ok...Any thing else");
+   printf("\n");
+
+    }
+    if(w>0)
+  {
+   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+   printf("\n");
+  hotel();
+    printf("enter your choice :");
+    scanf("%d",&a);
+  }
+
+
+    if(q==1)
+    {
+       printf("you have selected samosa\n");
+    }
+    else if(q==2)
+    {
+       printf("you have selected dosa\n");
+    }
+    else if(q==3)
+    {
+       printf("you have selected tea\n");
+    }
+    else if(q==4)
+    {
+       printf("you have selected coffee\n");
+
+    }
+    else
+    {
+        printf("\n");
+    }
+
+   printf("\n");
+   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+   printf("\n");
+   if(q==0)
    {
-    stu();
- printf("enter your choice\n");
- scanf("%d",&h);
-   }
-
-
-
-
-if(h==2)
-{
-
- for(i=0;i<g;i++)
- {
- printf("\n\t\t|| ALL STUDENT ||\n");
- printf("\nROLL NO.\tNAME\t\tDsu\tOOP\tDBMS\n");
- printf("\n%d\t\t%s\t\t%d\t%d\t%d\n",a[i].rollno,a[i].name,a[i].z,a[i].b,a[i].c);
- printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
- }
- if(h==0)
- {
-  printf("you selected have exit\n\nthank you!");
- }
- if(h>=2)
- {
- stu();
- printf("enter your choice\n");
- scanf("%d",&j);
- }
-}
-if(j==3)
-{
-for(i=0;i<g;i++)
-{
-    printf("\nROLL NO.\tNAME\t\tDsu\tOOP\tDBMS\n");
-    printf("\n%d\t\t%s\t\t%d\t%d\t%d\n",a[i].rollno,a[i].name,a[i].z,a[i].b,a[i].c);
-    printf("total marks scored by student =%d\n",sum=a[i].z+a[i].b+a[i].c);
-    per=sum;
-    printf("total percentage scored by student=%0.2f\n",per/300*100);
-
-    if(35<per)
-    {
-        printf("PASS\n");
+   printf("you have selected exit\n");
+   printf("samosa  %d*%d\n",y,b);
+   printf("dosa %d*%d\n",amt,n);
+   printf("tea %d*%d\n",o,g);
+   printf("coffee  %d*%d\n",p,h);
+    printf("bill=%d",sum);
+    printf("\n");
+ printf("enter amount ");
+    scanf("%d",&token);
+    printf("\n");
     }
-    else if(35>per)
+    if(sum==token)
     {
-        printf("FAIL\n");
+    printf("thank you \n visit again\n");
     }
-    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+   else
+   if(sum>token)
+    {
+   printf("please pay more  = 10\n");
+       printf("enter amount:  ");
+        scanf("%d",&pay);
+       printf("thank you \n visit again\n");
+    }
+    else
+    if(sum<token)
+    {
+     printf("return :10\n");
+     printf("enter the amount\n");
+     scanf("%d",&eamt);
+    printf("thank you \n visit again\n");
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
 }
-if(j==0)
-{
-    printf("you selected have exit\n\nthank you!");
-}
-if(j>=3)
-{
-stu();
-printf("enter your choice\n");
- scanf("%d",&h);
-}
-}
-if(h==4)
-{
-  printf("Do you want to change name:\n");
- printf("1.YES\n");
- printf("2.NO\n");
- scanf("%d",&v);
-}
-
- if(v==1)
- {
-     printf("choice 1\n");
- }
- else if(v==2)
- {
-     printf("choice 2\n");
- }
-
- if(v=1)
- {
- printf("enter your Roll No:\n");
- scanf("%d",&m);
- }
- for(i=0;i<g;i++)
- {
-    if(m==a[i].rollno)
-     {
-         printf("the old name of student is: %s\n",a[i].name);
-
-     }
-
- }
-
- if(v=2)
-
-printf("enter the new name of student\n");
-scanf("%s",a[i].name);
-printf("Do you really to change name:\n");
- printf("1.YES\n");
- printf("2.NO\n");
- scanf("%d",&v);
-
- if(v==1)
- {
-     printf("choice 1\n");
-     printf("\n\nok records updatted succesfully!\n");
- }
- else if(v==2)
- {
-     printf("choice 2\n");
- }
-
-
-printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-if(h>=4)
-{
-stu();
-printf("enter your choice\n");
- scanf("%d",&f);
-}
-if(f==1)
-printf("you have selected to add student\n");
-else if(f==2)
-printf("you have selected show all student\n");
-else if(f==3)
-printf("you have selected show result\n");
-else if(f==4)
-printf("you have selected update student data\n");
-else if(f==0)
-printf("you selected have exit\n\nthank you!");
-
-
-
- return 0;
-}
-
-
